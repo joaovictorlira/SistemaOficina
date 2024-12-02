4ff9a8bd68b955d67743420b740be34b2ed8917f
Caiosouto11 <caiosouto006@gmail.com>
2024-12-01 23:52:37 -0300

    Add files via upload

--- ProjetoFinalJava - Copia/Main.java
+++ ProjetoFinalJava - Copia/Main.java
@@ -1,20 +1,145 @@
 import java.util.Date;
+<<<<<<< HEAD
 
 import src.models.Servico;
 import src.models.TipoServiço;
+=======
+import src.models.*;
+import java.util.Scanner;
+>>>>>>> 91e78b2 (segundaversao)
 
 public class Main {
 
     public static void main(String[] args) {
+<<<<<<< HEAD
         
 
         Servico servico1 = new Servico(new Date(), "500", TipoServiço.BASICO);
 
 
         System.out.println(servico1);
+=======
+
+        Servico servico1 = new Servico(new Date(), "500", TipoServiço.BASICO);
+        System.out.println("Serviço criado: " + servico1);
+
+        Mecanico mecanico1 = new Mecanico("Caio Souto", "4002-8922", "026.321.467-46", "2000");
+        Borracheiro borracheiro1 = new Borracheiro("Yuri", "4002-8926", "238.345.453-45", "1500");
+
+
+        Scanner sc = new Scanner(System.in);
+
+        System.out.println("Digite 0 para Cliente e 1 para Funcionário");
+        int escolha = sc.nextInt(); // Solicita a escolha inicial
+
+        switch (escolha) {
+
+            case 0:
+                System.out.println("Digite 0 para ver a Nota Fiscal e 1 para realizar serviço");
+                int clienteescolha = sc.nextInt();
+
+                switch (clienteescolha) {
+                    case 0:
+                        System.out.println("Exibindo a nota fiscal");
+                        break;
+
+                    case 1:
+                        System.out.println("Qual serviço você deseja realizar?");
+                        System.out.println("Digite 0 se quiser trocar óleo\nDigite 1 se quiser trocar peças\nDigite 2 se quiser trocar o pneu\nDigite 3 se quiser remendar o furo do pneu");
+                        int servicoEscolha = sc.nextInt();
+
+                        switch (servicoEscolha) {
+                            case 0:
+                                System.out.println("Realizando troca de óleo...");
+                                mecanico1.trocarOleo();
+                                break;
+
+                            case 1:
+                                System.out.println("Realizando troca de peças...");
+                                mecanico1.trocarpecas();
+                                break;
+
+                            case 2:
+                                System.out.println("Realizando troca de pneu...");
+                                borracheiro1.trocarpneu();
+                                break;
+
+                            case 3:
+                                System.out.println("Remendando o furo do pneu...");
+                                borracheiro1.tamparFuroDoPneu();
+                                break;
+
+                            default:
+                                System.out.println("Opção inválida para o serviço.");
+                                break;
+                        }
+                        break;
+
+                    default:
+                        System.out.println("Opção inválida para Cliente.");
+                        break;
+                }
+                break;
+
+            case 1:
+                System.out.println("Digite 0 para Mecânico e 1 para Borracheiro");
+                int funcEscolha = sc.nextInt();
+
+
+                switch (funcEscolha) {
+                    case 0:
+                        System.out.println("Digite 0 se você quer trocar óleo ou 1 se você quer trocar peças");
+
+                        int escolhamecanico = sc.nextInt();
+
+                        switch (escolhamecanico){
+
+                            case 0:
+                                System.out.println("Você trocou o óleo do cliente!");
+                                break;
+
+                            case 1:
+                                System.out.println("Você trocou as peças do cliente!");
+                                break;
+
+                        }
+                        break;
+                    case 1:
+                        System.out.println("Digite 0 se quiser trocar pneu ou digite 1 se quiser remendar um furo no pneu.");
+                        int escolhaborracheiro = sc.nextInt();
+
+                        switch (escolhaborracheiro){
+
+                            case 0:
+                                System.out.println("Você trocou o pneu do cliente");
+                                break;
+
+                            case 1:
+                                System.out.println("Você remendou um furo no pneu do cliente");
+                        }
+>>>>>>> 91e78b2 (segundaversao)
 
 
 
 
+<<<<<<< HEAD
     }
 }
+=======
+                        break;
+
+                    default:
+                        System.out.println("Opção inválida para Funcionário.");
+                        break;
+                }
+                break;
+
+            default:
+                System.out.println("Opção inválida. Tente novamente.");
+                break;
+        }
+
+        sc.close(); // Fecha o scanner para evitar vazamentos de recursos
+    }
+}
+>>>>>>> 91e78b2 (segundaversao)
